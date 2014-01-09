7dc1c7235487f90136f0ec77b024d7d1cc3ca793
Michael Hart <mikeyman@hotmail.co.uk>
2014-01-09 16:36:11 +0000

    First Commit

---
+++ MrCube/src/net/cube/MrCube.java
@@ -0,0 +1,20 @@
+package net.cube;
+
+import com.jme3.app.SimpleApplication;
+
+public class MrCube extends SimpleApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		MrCube app = new MrCube();
+		app.start();
+	}
+
+	@Override
+	public void simpleInitApp() {
+		
+	}
+
+}
